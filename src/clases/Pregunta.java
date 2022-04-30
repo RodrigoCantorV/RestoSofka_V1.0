00c419626d75287bc7a7e9a0ec71dd2e6f601dab
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 17:32:23 -0500

    Estableciendo metodos const,get,set de la clase Pregunta

--- src/clases/Pregunta.java
+++ src/clases/Pregunta.java
@@ -2,4 +2,51 @@ package clases;
 
 public class Pregunta {
 
+	private int pre_id;
+	private int pre_id_cate;
+	private String interrogante;
+	private static int idSiguiente = 26;
+
+	public Pregunta(int pre_id, int pre_id_cate, String interrogante) {
+
+		this.pre_id = pre_id;
+		this.pre_id_cate = pre_id_cate;
+		this.interrogante = interrogante;
+	}
+
+	public Pregunta(int pre_id_cate, String interrogante) {
+		pre_id = idSiguiente;
+		idSiguiente++;
+		this.pre_id_cate = pre_id_cate;
+		this.interrogante = interrogante;
+	}
+
+	public Pregunta() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getPre_id() {
+		return pre_id;
+	}
+
+	public void setPre_id(int pre_id) {
+		this.pre_id = pre_id;
+	}
+
+	public int getPre_id_cate() {
+		return pre_id_cate;
+	}
+
+	public void setPre_id_cate(int pre_id_cate) {
+		this.pre_id_cate = pre_id_cate;
+	}
+
+	public String getInterrogante() {
+		return interrogante;
+	}
+
+	public void setInterrogante(String interrogante) {
+		this.interrogante = interrogante;
+	}
+
 }
