f4f07d9aecdf624d631fd9caeffb0536780c8867
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 17:46:13 -0500

    Estableciendo metodos conectarse() y desconectar()

--- src/conexionBD/ConexionMySQL.java
+++ src/conexionBD/ConexionMySQL.java
@@ -1,5 +1,39 @@
 package conexionBD;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexionMySQL {
 
+	static String bd = "juego_sofka";
+	static String login = "root";
+	static String password = "";
+	static String url = "jdbc:mysql://localhost/" + bd;
+
+	Connection conn = null;
+
+	public void conectarse() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(url, login, password);
+			if (conn != null) {
+				// System.out.println("Conexion a la base de datos " + bd + " esta lista.");
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+			System.out.println("error base de datos desconocida o el servidos no esta en ejecucion");
+		} catch (ClassNotFoundException e) {
+			System.out.println(e);
+		}
+	}
+
+	public Connection getConnection() {
+		return conn;
+	}
+
+	public void desconectar() {
+		conn = null;
+		// System.out.println("Conexion a la base de datos " + bd + " esta cerrada");
+	}
 }
