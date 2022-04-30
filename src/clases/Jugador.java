ade8e600deda6b9433ac6c85d616ba6337eab255
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 17:25:11 -0500

    Estableciendo metodos cons,get,set de la clase Jugador

--- src/clases/Jugador.java
+++ src/clases/Jugador.java
@@ -1,5 +1,33 @@
 package clases;
 
 public class Jugador {
+	private int jugador_id;
+	private String jugador_nom;
+
+	public Jugador() {
+		// this.jugador_id = jugador_id;
+		this.jugador_nom = jugador_nom;
+	}
+
+	public Jugador(int jugador_id, String jugador_nom) {
+		// this.jugador_id = jugador_id;
+		this.jugador_nom = jugador_nom;
+	}
+
+	public int getJugador_id() {
+		return jugador_id;
+	}
+
+	public void setJugador_id(int jugador_id) {
+		this.jugador_id = jugador_id;
+	}
+
+	public String getJugador_nom() {
+		return jugador_nom;
+	}
+
+	public void setJugador_nom(String jugador_nom) {
+		this.jugador_nom = jugador_nom;
+	}
 
 }
