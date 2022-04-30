559f64568aec01236a466c3793e04db276cace04
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 17:41:51 -0500

    Estableciendo metodos cons,set,get de la clase Ronda_Partida

--- src/clases/Ronda_Partida.java
+++ src/clases/Ronda_Partida.java
@@ -1,5 +1,32 @@
 package clases;
 
 public class Ronda_Partida {
+	private int id_ronda;
+	private int id_partida;
 
+	public Ronda_Partida(int id_ronda, int id_partida) {
+		this.id_ronda = id_ronda;
+		this.id_partida = id_partida;
+	}
+
+	public Ronda_Partida() {
+		this.id_ronda = id_ronda;
+		this.id_partida = id_partida;
+	}
+
+	public int getId_ronda() {
+		return id_ronda;
+	}
+
+	public void setId_ronda(int id_ronda) {
+		this.id_ronda = id_ronda;
+	}
+
+	public int getId_partida() {
+		return id_partida;
+	}
+
+	public void setId_partida(int id_partida) {
+		this.id_partida = id_partida;
+	}
 }
