9640298b4cdb559765a83e74c212ed8dbee73e91
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 18:23:53 -0500

    Estableciendo funciones de la interface IPreguntaDAO

--- src/interfaces/IPreguntaDAO.java
+++ src/interfaces/IPreguntaDAO.java
@@ -1,5 +1,18 @@
 package interfaces;
 
+import clases.Pregunta;
+
 public interface IPreguntaDAO {
 
+	public void crearPregunta(Pregunta objPregunta);
+
+	public void modificarPregunta(Pregunta objPregunta);
+
+	public void eliminarPregunta(Pregunta objPregunta);
+
+	public void listarPregunta();
+
+	public Pregunta[] listarPreguntaAletoriaCategoria(int cate);
+
+	public int obtenerultimaPregunta();
 }
