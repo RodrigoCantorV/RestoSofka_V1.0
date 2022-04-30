5ef1082a6a3d531427964fa342bf87874f663bd0
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 18:18:18 -0500

    Estableciendo funciones de la Interfaz IPartidaDAO

--- src/interfaces/IPartidaDAO.java
+++ src/interfaces/IPartidaDAO.java
@@ -1,5 +1,11 @@
 package interfaces;
 
-public interface IPartidaDAO {
+import clases.Partida;
 
+public interface IPartidaDAO {
+	public void crearPartida(Partida objPartida);
+
+	public void listarPartida();
+
+	public int obtenerPartida(); // esta accion me va a retornar al ultima partida creada
 }
