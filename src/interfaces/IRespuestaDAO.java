e9ccaaa08b83254842db8ef57407e934ddc3762a
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 18:26:45 -0500

    Estableciendo funciones de la interface IRespuestaDAO

--- src/interfaces/IRespuestaDAO.java
+++ src/interfaces/IRespuestaDAO.java
@@ -1,5 +1,15 @@
 package interfaces;
 
-public interface IRespuestaDAO {
+import clases.Respuesta;
 
+public interface IRespuestaDAO {
+	public void crearRespuesta(Respuesta objRespuesta);
+
+	public void modificarRespuesta(Respuesta objRespuesta);
+
+	public void eliminarRespuesta(Respuesta objRespuesta);
+
+	public void listarRespuestaPorID(int id);
+
+	public Respuesta[] ObtenerRespuestaPorID(int id);
 }
