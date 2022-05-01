482a1f30dcc40ac3e26e11d99d87ac1b232c587d
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 19:00:41 -0500

    Desarrollando funcion Ronda_PartidaDAO

--- src/dao/Ronda_PartidaDAO.java
+++ src/dao/Ronda_PartidaDAO.java
@@ -1,5 +1,29 @@
 package dao;
 
-public class Ronda_PartidaDAO {
+import java.sql.SQLException;
+import com.mysql.jdbc.PreparedStatement;
+import clases.Ronda_Partida;
+import conexionBD.ConexionMySQL;
+import interfaces.IRonda_PartidaDAO;
 
+public class Ronda_PartidaDAO implements IRonda_PartidaDAO {
+
+	@Override
+	public void crearRondaPartida(Ronda_Partida objetoRonda_Partida) {
+		ConexionMySQL con = new ConexionMySQL();
+		con.conectarse();
+		try {
+			PreparedStatement pstm = (PreparedStatement) con.getConnection()
+					.prepareStatement("INSERT INTO rondapartida(id_ronda,id_partida) VALUES(?,?)");
+			pstm.setInt(1, objetoRonda_Partida.getId_ronda());
+			pstm.setInt(2, objetoRonda_Partida.getId_partida());
+			pstm.execute();
+			System.out.println("Registros insertados correctamente");
+			pstm.close();
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		con.desconectar();
+
+	}
 }
