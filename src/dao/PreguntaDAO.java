3dd19701534b5866afc444066a1eaedb1394aa77
RODRIGO <RODRIGO@LAPTOP-6H245NPA>
2022-04-30 20:08:11 -0500

    Finalizacion Juego

--- src/dao/PreguntaDAO.java
+++ src/dao/PreguntaDAO.java
@@ -2,25 +2,26 @@ package dao;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
 import com.mysql.jdbc.PreparedStatement;
+
 import clases.Pregunta;
 import conexionBD.ConexionMySQL;
 import interfaces.IPreguntaDAO;
 
-public class PreguntaDAO implements IPreguntaDAO {
+public class PreguntaDAO implements IPreguntaDAO  {
 
 	static int ultimapregunta;
 	static Pregunta[] miPregunta;
-
+	
 	@Override
 	public void crearPregunta(Pregunta objPregunta) {
 		ConexionMySQL con = new ConexionMySQL();
 		con.conectarse();
 		try {
-			PreparedStatement pstm = (PreparedStatement) con.getConnection()
-					.prepareStatement("INSERT INTO pregunta(id_categoria,interrogante) VALUES(?,?)");
-			pstm.setInt(1, objPregunta.getPre_id_cate());
-			pstm.setString(2, objPregunta.getInterrogante());
+			PreparedStatement pstm = (PreparedStatement) con.getConnection().prepareStatement("INSERT INTO pregunta(id_categoria,interrogante) VALUES(?,?)");
+			pstm.setInt(1,objPregunta.getPre_id_cate());
+			pstm.setString(2,objPregunta.getInterrogante());
 			pstm.execute();
 			System.out.println("Registros insertados correctamente");
 			pstm.close();
@@ -35,12 +36,11 @@ public class PreguntaDAO implements IPreguntaDAO {
 		ConexionMySQL con = new ConexionMySQL();
 		con.conectarse();
 		try {
-			PreparedStatement pstm = (PreparedStatement) con.getConnection()
-					.prepareStatement("UPDATE pregunta SET id_categoria =?,interrogante=? WHERE id_pregunta=?");
-			pstm.setInt(1, objPregunta.getPre_id_cate());
-			pstm.setString(2, objPregunta.getInterrogante());
-			pstm.setInt(3, objPregunta.getPre_id());
-
+			PreparedStatement pstm = (PreparedStatement) con.getConnection().prepareStatement("UPDATE pregunta SET id_categoria =?,interrogante=? WHERE id_pregunta=?");
+			pstm.setInt(1,objPregunta.getPre_id_cate());
+			pstm.setString(2,objPregunta.getInterrogante());
+			pstm.setInt(3,objPregunta.getPre_id());
+			
 			pstm.execute();
 			System.out.println("Registro modificado correctamente");
 			pstm.close();
@@ -48,7 +48,7 @@ public class PreguntaDAO implements IPreguntaDAO {
 			System.out.println(e);
 		}
 		con.desconectar();
-
+		
 	}
 
 	@Override
@@ -56,9 +56,8 @@ public class PreguntaDAO implements IPreguntaDAO {
 		ConexionMySQL con = new ConexionMySQL();
 		con.conectarse();
 		try {
-			PreparedStatement pstm = (PreparedStatement) con.getConnection()
-					.prepareStatement("DELETE from pregunta WHERE id_pregunta =?");
-			pstm.setInt(1, objPregunta.getPre_id());
+			PreparedStatement pstm = (PreparedStatement) con.getConnection().prepareStatement("DELETE from pregunta WHERE id_pregunta =?");
+			pstm.setInt(1,objPregunta.getPre_id());
 			pstm.execute();
 			System.out.println("Registro Eliminado correctamente");
 			pstm.close();
@@ -66,7 +65,7 @@ public class PreguntaDAO implements IPreguntaDAO {
 			System.out.println(e);
 		}
 		con.desconectar();
-
+		
 	}
 
 	@Override
@@ -79,11 +78,10 @@ public class PreguntaDAO implements IPreguntaDAO {
 			System.out.println("Lista de preguntas: \n");
 			System.out.println("N° pregunta" + "\tcategoria" + "\tInterrogante");
 			System.out.println("------------------------------------------------");
-			while (rs.next()) {
-				System.out.println("\t" + rs.getInt("id_pregunta") + " \t" + rs.getInt("id_categoria") + "\t\t "
-						+ rs.getString("interrogante"));
+			while(rs.next()) {
+				System.out.println("\t"+rs.getInt("id_pregunta") + " \t" + rs.getInt("id_categoria") + "\t\t " + rs.getString("interrogante"));
 			}
-
+			
 			pstm.execute();
 			System.out.println("Registros listados correctamente");
 			pstm.close();
@@ -91,28 +89,27 @@ public class PreguntaDAO implements IPreguntaDAO {
 			System.out.println(e);
 		}
 		con.desconectar();
-
+		
 	}
 
 	@Override
 	public int obtenerultimaPregunta() {
-
+		
 		ConexionMySQL con = new ConexionMySQL();
 		con.conectarse();
 		try {
-			PreparedStatement pstm = (PreparedStatement) con.getConnection()
-					.prepareStatement("SELECT count(*)  as cantidad FROM pregunta");
+			PreparedStatement pstm = (PreparedStatement) con.getConnection().prepareStatement("SELECT count(*)  as cantidad FROM pregunta");
 			ResultSet rs = (ResultSet) pstm.executeQuery();
 			rs.next();
-			ultimapregunta = rs.getInt("cantidad");
-
+			ultimapregunta= rs.getInt("cantidad");
+			
 			pstm.execute();
 			System.out.println("Registros listados correctamente");
 			pstm.close();
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
-
+		
 		con.desconectar();
 		return ultimapregunta;
 	}
@@ -122,20 +119,18 @@ public class PreguntaDAO implements IPreguntaDAO {
 		ConexionMySQL con = new ConexionMySQL();
 		con.conectarse();
 		try {
-			PreparedStatement pstm = (PreparedStatement) con.getConnection().prepareStatement(
-					"SELECT id_pregunta,id_categoria,interrogante FROM pregunta WHERE id_categoria = " + cate);
+			PreparedStatement pstm = (PreparedStatement) con.getConnection().prepareStatement("SELECT id_pregunta,id_categoria,interrogante FROM pregunta WHERE id_categoria = "+ cate);
 			ResultSet rs = (ResultSet) pstm.executeQuery();
-
-			miPregunta = new Pregunta[5];
+			
+			 miPregunta = new Pregunta[5];
 			for (int i = 0; i < miPregunta.length; i++) {
 				rs.next();
-				// miPregunta[i] = new Pregunta(cate, rs.getString("interrogante"));
-				miPregunta[i] = new Pregunta(rs.getInt("id_pregunta"), rs.getInt("id_categoria"),
-						rs.getString("interrogante"));
+				//miPregunta[i] = new Pregunta(cate, rs.getString("interrogante"));	
+				miPregunta[i] = new Pregunta(rs.getInt("id_pregunta"),rs.getInt("id_categoria"),rs.getString("interrogante"));	
 			}
-
+			
 			pstm.execute();
-			// System.out.println("Registros listados correctamente");
+		//	System.out.println("Registros listados correctamente");
 			pstm.close();
 		} catch (SQLException e) {
 			System.out.println(e);
@@ -143,4 +138,6 @@ public class PreguntaDAO implements IPreguntaDAO {
 		con.desconectar();
 		return miPregunta;
 	}
+
+
 }
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,23 +1,58 @@
 package principal;
 
+import java.util.Random;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+import clases.Jugador;
+import clases.Partida;
 import clases.Pregunta;
 import clases.Respuesta;
 import clases.ResumenPartida;
+import clases.Ronda_Partida;
+import dao.CategoriaDAO;
+import dao.JugadorDAO;
+import dao.PartidaDAO;
 import dao.PreguntaDAO;
 import dao.RespuestaDAO;
+import dao.ResumenPartidaDAO;
+import dao.Ronda_PartidaDAO;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		int opcion1, opcion2;
-
+		int contadorCategoria;
+		int respuestaAleatoria;
+		String continuarJugando;
+		boolean finalizar;
+		int seleccionarRespuesta;
+		int puntajePartida = 0;
+		Random r = new Random();
+		ArrayList<ResumenPartida>  miResumen;
+		
 		Pregunta objPregunta = new Pregunta();
 		PreguntaDAO objPreguntaDao = new PreguntaDAO();
 
 		Respuesta objRespuesta = new Respuesta();
 		RespuestaDAO objRespuestaDAO = new RespuestaDAO();
+
+		CategoriaDAO objCategoriaDAO = new CategoriaDAO();
+
+		Jugador obJugador = new Jugador();
+		JugadorDAO obJugadorDAO = obJugadorDAO = new JugadorDAO();
+
+		Partida objPartida = new Partida();
+		PartidaDAO objPartidaDAO = new PartidaDAO();
+
+		Ronda_Partida objRonda_Partida = new Ronda_Partida();
+		Ronda_PartidaDAO objRonda_PartidaDAO = new Ronda_PartidaDAO();
+		
+		ResumenPartida objResumenPartida = new ResumenPartida();
+		ResumenPartidaDAO  objResumenPartidaDAO = new ResumenPartidaDAO();
+		
+		miResumen = objResumenPartidaDAO.resumen();
 
 		do {
 			System.out.println();
@@ -31,6 +66,75 @@ public class Principal {
 			opcion1 = entrada.nextInt();
 			switch (opcion1) {
 			case 1:
+				contadorCategoria = 1;
+				finalizar = false;
+				puntajePartida =0;
+				// creamos el jugador 
+				System.out.print("Ingresa tu Nickname: ");
+				obJugador.setJugador_nom(entrada.next());
+				obJugadorDAO.crearJugador(obJugador);
+
+				// listar pregunta por categoria
+
+				do {
+					Pregunta PreguntaAleatoria[];
+					Respuesta MisRespuestas[];
+					respuestaAleatoria = (int) (Math.random() * 5 + 0);
+					PreguntaAleatoria = objPreguntaDao.listarPreguntaAletoriaCategoria(contadorCategoria);
+					System.out.println();
+					System.out.println("Pregunta " + contadorCategoria + " categoria " + contadorCategoria + " \n");
+					System.out.println(PreguntaAleatoria[respuestaAleatoria].getInterrogante());
+
+					MisRespuestas = objRespuestaDAO
+							.ObtenerRespuestaPorID(PreguntaAleatoria[respuestaAleatoria].getPre_id());
+					// revolver respuestas
+					for (int i = 0; i < MisRespuestas.length; i++) {
+						int posAleatoria = r.nextInt(MisRespuestas.length);
+						Respuesta temp = MisRespuestas[i];
+						MisRespuestas[i] = MisRespuestas[posAleatoria];
+						MisRespuestas[posAleatoria] = temp;
+					}
+					// mostrar respuestas
+					for (int i = 0; i < MisRespuestas.length; i++) {
+						System.out.println(i + ".) " + MisRespuestas[i].getSolucion());
+					}
+					// Seleccionar respuesta correcta
+
+					System.out.print("Escribe el numero de la respuesta: ");
+					seleccionarRespuesta = entrada.nextInt();
+					entrada.nextLine();
+					// System.out.println(MisRespuestas[seleccionarRespuesta].getRespuesta_correcta());
+					if (MisRespuestas[seleccionarRespuesta].getRespuesta_correcta() == 1) {
+						System.out.println("Respuesta Correcta!");
+						puntajePartida += objCategoriaDAO.obtenerPuntaje(contadorCategoria);
+						System.out.println(puntajePartida);
+					} else {
+						System.out.println("Incorrecta");
+						puntajePartida = 0;
+						finalizar = true;
+					}
+
+					if (contadorCategoria != 5 && finalizar == false) {
+						System.out.println("¿Deseas Continuar Jugando? Y/N: ");
+						continuarJugando = entrada.nextLine();
+						if (continuarJugando.equalsIgnoreCase("Y")) {
+							contadorCategoria++;
+						} else {
+							finalizar = true;
+						}
+					} else {
+						finalizar = true;
+					}
+
+				} while (finalizar != true);
+
+				objPartida.setJugador_id(obJugadorDAO.obtenerUltimoJugador());
+				objPartida.setAcumulado_partida(puntajePartida);
+				objPartidaDAO.crearPartida(objPartida);
+
+				objRonda_Partida.setId_partida(objPartidaDAO.obtenerPartida());
+				objRonda_Partida.setId_ronda(contadorCategoria);
+				objRonda_PartidaDAO.crearRondaPartida(objRonda_Partida);
 
 				break;
 			case 2:
@@ -44,7 +148,7 @@ public class Principal {
 					System.out.println("* 4.)MODIFICAR RESPUESTA*************");
 					System.out.println("* 5.)LISTAR PREGUNTA Y SU RESPUESTA**");
 					System.out.println("* 6.)LISTAR CATEGORIAS EXISTENTES****");
-					System.out.println("* 7.)LISTAR RESUMEN DE PARTIDAS******");
+					System.out.println("* 7.)LISTAR RESUMEN DE PARTIDAS****");
 					System.out.println("* 8.)ATRAS***************************");
 					System.out.println("*************************************");
 					System.out.print("Escribe la opcion deseada: ");
@@ -89,16 +193,42 @@ public class Principal {
 
 					case 4:
 
+						System.out.print("¿A que pregunta deseas modificar una de sus respuestas?: ");
+						int id_pregunta = entrada.nextInt();
+						entrada.nextLine();
+						objRespuestaDAO.listarRespuestaPorID(id_pregunta);
+						objRespuesta.setId_pregunta(id_pregunta);
+
+						System.out.print("Establezca el numero de la respuesta que desea modificar: ");
+						objRespuesta.setRespuesta_id(entrada.nextInt());
+						entrada.nextLine();
+						System.out.print("Escribe el nuevo contenido para la respuesta: ");
+						objRespuesta.setSolucion(entrada.nextLine());
+						objRespuestaDAO.modificarRespuesta(objRespuesta);
+
 						break;
 
 					case 5:
-
+						System.out.print("Que pregunta deseas ver: ");
+						objRespuestaDAO.listarRespuestaPorID(entrada.nextInt());
+						entrada.hasNextLine();
 						break;
 					case 6:
-
+						objCategoriaDAO.listarCategoria();
 						break;
 					case 7:
-
+						
+						
+						System.out.println("id_partida \t" +"nom_jugador \t" + "ronda_maxima \t" + "    acumulado \t" );
+						System.out.println("_________________________________________________________________________");
+						for(ResumenPartida e: miResumen) {
+							//System.out.println(e.getId_partida() + " " + e.getNom_jugador() + " " +  e.getRonda_maxima() + " " + e.getCumulado_final());
+							System.out.print(e.getId_partida() + "\t\t ");
+							System.out.print(e.getNom_jugador()  + "\t\t ");
+							System.out.print(e.getRonda_maxima()  + "\t\t ");
+							System.out.print(e.getCumulado_final()  + "\t\t ");
+							System.out.println();
+						}
 						break;
 					default:
 						System.out.println("Esta opcion no es valida Por favor intenta de nuevo!");
@@ -114,6 +244,7 @@ public class Principal {
 		} while (opcion1 != 3);
 
 		System.out.println("Has salido del juego...Gracias");
+
 	}
 
 }
